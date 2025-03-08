1da9e0bdd2ee01e46affce986daf821546511755
turaltahmazli <turalft-bp218@code.edu.az>
2025-03-08 21:52:17 +0400

    upgraded tasks

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,134 +1,159 @@
-﻿namespace ConsoleApp1
+﻿using System.Text;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
-        private static void task1(int num1, int num2)
+        #region task1
+        private static double SumTwoNumbers(double num1, double num2)
         {
-            Console.WriteLine(num1 + num2);
+            return num1 + num2;
         }
-        private static void task2(int num1)
+        #endregion
+
+        #region task2
+        /// <summary>
+        /// Determines if a number is positive, negative, or zero.
+        /// </summary>
+        /// <param name="num">The number to check.</param>
+        /// <returns>
+        /// 1 if the number is positive,
+        /// 0 if the number is negative,
+        /// -1 if the number is zero.
+        /// </returns>
+        private static sbyte IsPosetive(double num)
         {
-            if (num1 > 0)
-            {
-                Console.WriteLine("Positive");
-                return;
-            }
-            if (num1 < 0)
-            {
-                Console.WriteLine("Negative");
-                return;
-            }
-            Console.WriteLine("Zero");
-            return;
+            if (num > 0)
+                return 1;
+            if (num < 0)
+                return 0;
+            return -1;
         }
-        private static void task3(int num1, int num2, int num3)
+        #endregion
+
+        #region task3
+        //method 1
+        private static double GetMaxOfThreeNumbers(double num1, double num2, double num3)
         {
-            List<int> numbers = new List<int> { num1, num2, num3 };
-            int max = numbers[0];
+            if (num1 >= num2)
+            {
+                if (num1 >= num3)
+                    return num1;
+                return num3;
+            }
+            if (num2 >= num3)
+                return num2;
+            return num3;
+        }
+        //method 2
+        private static double GetMax(params double[] numbers)
+        {
+            Index maxIndex = 0;
             for (int i = 1; i < numbers.Count(); i++)
-                if (max < numbers[i])
-                    max = numbers[i];
-            Console.WriteLine(max);
+                if (numbers[maxIndex] < numbers[i])
+                    maxIndex = i;
+            return numbers[maxIndex];
         }
-        private static void task4(int num1)
+        //method 2
+        private static double GetMax2(params double[] numbers)
         {
-            Console.Write($"{num1} is ");
-            for (int i = 2; i < Math.Sqrt(num1); i++)
-            {
-                if (num1 % i == 0)
-                {
-                    Console.WriteLine("not prime");
-                    return;
-                }
-            }
-            Console.WriteLine("prime");
+            Array.Sort(numbers);
+            return numbers[^1];
         }
-        private static void task5(int num1)
+        #endregion
+
+        #region task4
+        private static bool IsPrime(int num)
         {
-            //int f0 = 0;
-            //int f1 = 1;
-            //int i = 2;
-            //while (i < num1 + 2)
-            //{
-            //    Console.WriteLine(f0);
-            //    int temp = f0;
-            //    f0 = f1;
-            //    f1 = temp + f0;
-            //    i += 1;
-            //}
-            int f0 = 0;
-            int f1 = 1;
-            int i = 2;
-            while (i < num1 + 2)
+            int absNum = Math.Abs(num);
+            if (absNum == 1 || absNum == 0)
+                return false;
+            int length = (int)Math.Sqrt(absNum);
+            for (int i = 2; i <= length; i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+        #endregion
+
+        #region task5
+        private static List<int>? FibofibonacciNumbers(int num)
+        {
+            if (num < 0)
+                return null;
+            if (num == 1)
+                return new List<int>() { 0 };
+            int f0 = 0, f1 = 1;
+            List<int> fibNumbers = new List<int> { f0, f1 };
+            while (num > 2)
             {
-                Console.WriteLine(f0 + f1);
                 f1 += f0;
                 f0 = f1 - f0;
-                //int temp = f0;
-                //f0 = f1;
-                //f1 = temp + f0;
-                i += 1;
+                //(f0, f1) = (f1, f0 + f1);
+                fibNumbers.Add(f1);
+                num--;
             }
+            return fibNumbers;
         }
-        private static void task6(int num1)
-        {
-            int temp = num1;
-            int numberCount = -1;
-            //find  number count
-            while (temp != 0)
-            {
-                temp /= 10;
-                numberCount++;
-            }
-            //reverse number
-            double result = 0;
-            while (num1 != 0)
-            {
-                Math.Pow(10, numberCount);
-                result += (num1 % 10) * Math.Pow(10, numberCount);
-                num1 /= 10;
-                numberCount--;
-            }
+        #endregion
 
-            Console.WriteLine(result);
+        #region task6
+        private static int ReverseNumber(int num)
+        {
+            int result = 0;
+            while (num != 0)
+            {
+                result = result * 10 + (num % 10);
+                num /= 10;
+            }
+            return result;
         }
+        #endregion
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            int num1 = 1, num2 = 2, num3 = 8;
-            //Console.WriteLine("Enter three numbers: ");
-            //num1 = Convert.ToInt32(Console.ReadLine());
-            //num2 = Convert.ToInt32(Console.ReadLine());
-            //num3 = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-            Console.WriteLine($"num1 = {num1}, num2 = {num2}");
-            task1(num1, num2);
-            task2(num1);
-            task3(num1, num2, num3);
-            task4(num3);
-            task5(num3);
-            int num4 = 123451;
-            task6(num4);
+            // Test SumTwoNumbers
+            double sumResult = SumTwoNumbers(3.5, 2.5);
+            Console.WriteLine($"SumTwoNumbers(3.5, 2.5) = {sumResult}"); // Expected: 6.0
+            Console.WriteLine();
 
-            //if (num1 > num2)
-            //{
-            //    if (num1 > num3)
-            //    {
-            //        Console.WriteLine(num1);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(num3);
-            //    }
-            //}
-            //else if (num2 > num3)
-            //{
-            //    Console.WriteLine(num2);
-            //}
-            //else
-            //{
-            //    Console.WriteLine(num3);
-            //}
+            // Test IsPosetive
+            sbyte posResult1 = IsPosetive(5);
+            sbyte posResult2 = IsPosetive(-3);
+            sbyte posResult3 = IsPosetive(0);
+            Console.WriteLine($"IsPosetive(5) = {posResult1}"); // Expected: 1
+            Console.WriteLine($"IsPosetive(-3) = {posResult2}"); // Expected: 0
+            Console.WriteLine($"IsPosetive(0) = {posResult3}"); // Expected: -1
+            Console.WriteLine();
+
+            // Test GetMaxOfThreeNumbers
+            double maxOfThreeResult = GetMaxOfThreeNumbers(1, 5, 3);
+            Console.WriteLine($"GetMaxOfThreeNumbers(1, 5, 3) = {maxOfThreeResult}"); // Expected: 5
+
+            // Test GetMax
+            double maxResult = GetMax(1, 5, 3, 7, 2);
+            Console.WriteLine($"GetMax(1, 5, 3, 7, 2) = {maxResult}"); // Expected: 7
+
+            // Test GetMax2
+            double maxResult2 = GetMax2(1, 5, 3, 7, 2);
+            Console.WriteLine($"GetMax2(1, 5, 3, 7, 2) = {maxResult2}"); // Expected: 7
+            Console.WriteLine();
+
+            // Test IsPrime
+            bool primeResult1 = IsPrime(7);
+            bool primeResult2 = IsPrime(4);
+            Console.WriteLine($"IsPrime(7) = {primeResult1}"); // Expected: True
+            Console.WriteLine($"IsPrime(4) = {primeResult2}"); // Expected: False
+            Console.WriteLine();
+
+            // Test FibofibonacciNumbers
+            var fibResult = FibofibonacciNumbers(5);
+            Console.WriteLine("FibofibonacciNumbers(5) = " + string.Join(", ", fibResult)); // Expected: 0, 1, 1, 2, 3
+            Console.WriteLine();
+
+            // Test ReverseNumber
+            int reverseResult = ReverseNumber(12345);
+            Console.WriteLine($"ReverseNumber(12345) = {reverseResult}"); // Expected: 54321
         }
+
     }
 }
